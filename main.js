e670ff7d66e6b25263b1839ff3759c0ed298b9c9
Andrei Strelchik <thestrelchik@gmail.com>
2022-02-05 23:02:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ function a(){
     start++;
 }
 setInterval(a,200);
+
+
+// 2 вариант
+// const counterElement = document.querySelector('.counter');
+// let counter = 0;
+
+
+// setInterval(function() {
+//     counter++;
+//     console.log(counter);
+// }, 1000)
+
+// setInterval(function() {
+//     counterElement.innerText = counter;
+// }, 1000)
